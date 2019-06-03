d243bae1b916dbbd6948686dcd1f30bef5d04b38
fakeProgrammer0 <greenjiachen@gmail.com>
2019-06-03 16:44:53 +0800

    add EncryptTest.java

---
+++ backend/src/test/java/cn/edu/scut/bookshop/config/EncryptTest.java
@@ -0,0 +1,26 @@
+package cn.edu.scut.bookshop.config;
+
+import cn.edu.scut.bookshop.BookshopApplicationTests;
+import org.jasypt.encryption.StringEncryptor;
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class EncryptTest extends BookshopApplicationTests
+{
+    @Autowired
+    private StringEncryptor encryptor;
+    
+    @Test
+    public void generateKeys()
+    {
+        String ENC_url = encryptor.encrypt("jdbc:mysql://107.191.52.220:3306/bookshop?requireSSL=true&useSSL=true&verifyServerCertificate=false&serverTimezone=Hongkong&useUnicode=true&characterEncoding=utf-8");
+        String ENC_username = encryptor.encrypt("root");
+        String ENC_password = encryptor.encrypt("root0101");
+        System.out.printf("url: ENC(%s)\n", ENC_url);
+        System.out.printf("username: ENC(%s)\n", ENC_username);
+        System.out.printf("password: ENC(%s)\n", ENC_password);
+        Assert.assertTrue(ENC_username.length() > 0);
+        Assert.assertTrue(ENC_password.length() > 0);
+    }
+}
